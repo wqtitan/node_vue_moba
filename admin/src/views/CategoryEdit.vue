e2e82ff4b2fd13775b760edf824eb18437fab9d1
wqtitan <wqtitan@gmail.com>
2020-06-10 14:58:45 +0800

    通用CRUD

--- admin/src/views/CategoryEdit.vue
+++ admin/src/views/CategoryEdit.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
-    <h1>新建分类</h1>
+    <h1>{{ id ? "编辑" : "新建" }}分类</h1>
     <el-form label-width="120px" @submit.native.prevent="save">
+      <el-form-item label="上级分类">
+        <el-select v-model="model.parent">
+          <el-option v-for="(item) in parents" :key="item._id" 
+          :label="item.name" :value="item._id"></el-option>
+        </el-select>
+      </el-form-item>
       <el-form-item label="名称">
         <el-input v-model="model.name"></el-input>
       </el-form-item>
@@ -16,18 +22,38 @@
 export default {
   data() {
     return {
-      model: {}
-    }
+      model: {},
+      parents: []
+    };
+  },
+  props: {
+    id: {}
   },
   methods: {
     async save() {
-      await this.$http.post('categories', this.model)
-      this.$router.push('/categories/list')
+      if (this.id) {
+        await this.$http.put(`rest/categories/${this.id}`, this.model);
+      } else {
+        await this.$http.post("rest/categories", this.model);
+      }
+      this.$router.push("/categories/list");
       this.$message({
-        type: 'success',
-        message: '保存成功'
-      })
+        type: "success",
+        message: "保存成功"
+      });
+    },
+    async fetch() {
+      const res = await this.$http.get(`rest/categories/${this.id}`);
+      this.model = res.data;
+    },
+    async fetchParents() {
+      const res = await this.$http.get(`rest/categories`);
+      this.parents = res.data;
     }
+  },
+  created() {
+    this.fetchParents();
+    this.id && this.fetch();
   }
-}
+};
 </script>
