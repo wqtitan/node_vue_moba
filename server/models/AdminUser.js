09b440f010582bb0f6ca5100d9fd4cbd76756b8c
wqtitan <wqtitan@gmail.com>
2020-06-11 15:29:18 +0800

    管理员账号管理

---
+++ server/models/AdminUser.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const { hashSync } = require('bcrypt');
+
+const schema = new mongoose.Schema({
+  username: { type: String },
+  password: {
+    type: String,
+    select: false,
+    set(val) {
+      return require('bcrypt'), hashSync(val, 12);
+    }
+  },
+});
+
+module.exports = mongoose.model('AdminUser', schema);
