2e7d832568a34b2cf7d027f43ef3b9e152d0ae26
wqtitan <wqtitan@gmail.com>
2020-06-12 19:53:35 +0800

    服务端校验

--- admin/src/http.js
+++ admin/src/http.js
@@ -1,9 +1,23 @@
 import axios from "axios";
 import Vue from "vue";
+import router from "./router";
 
 const http = axios.create({
   baseURL: "http://localhost:4000/admin/api",
 });
+
+http.interceptors.request.use(
+  function(config) {
+    if (localStorage.token) {
+      config.headers.Authorization = "Bearer " + localStorage.token;
+    }
+
+    return config;
+  },
+  function(error) {
+    return Promise.reject(error);
+  }
+);
 
 http.interceptors.response.use(
   (res) => {
@@ -15,6 +29,10 @@ http.interceptors.response.use(
         type: "error",
         message: err.response.data.message,
       });
+
+      if (err.response.status === 401) {
+        router.push("/login");
+      }
     }
     return Promise.reject(err);
   }
--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,75 +1,95 @@
-const AdminUser = require('../../models/AdminUser');
+const { assert } = require("console");
 
-module.exports = app => {
-  const express = require('express');
+module.exports = (app) => {
+  const express = require("express");
+  const AdminUser = require("../../models/AdminUser");
+  const jwt = require("jsonwebtoken");
+  const assert = require("http-assert");
   const router = express.Router({
-    mergeParams: true
+    mergeParams: true,
   });
 
-  router.post('/', async (req, res) => {
+  router.post("/", async (req, res) => {
     const model = await req.Model.create(req.body);
     res.send(model);
   });
-  router.put('/:id', async (req, res) => {
+  router.put("/:id", async (req, res) => {
     const model = await req.Model.findByIdAndUpdate(req.params.id, req.body);
     res.send(model);
   });
-  router.delete('/:id', async (req, res) => {
+  router.delete("/:id", async (req, res) => {
     await req.Model.findByIdAndDelete(req.params.id, req.body);
     res.send({
-      seccess: true
+      seccess: true,
     });
   });
-  router.get('/', async (req, res) => {
-    const queryOptions = {};
-    if (req.Model.modelName === 'Category') {
-      queryOptions.populate = 'parent';
+  router.get(
+    "/",
+    async (req, res, next) => {
+      const token = String(req.headers.authorization || "")
+        .split(" ")
+        .pop();
+      assert(token, 401, "请先登录");
+      const { id } = jwt.verify(token, app.get("secret"));
+      assert(id, 401, "请先登录");
+      req.user = await AdminUser.findById(id);
+      assert(req.user, 401, "请先登录");
+      await next();
+    },
+    async (req, res) => {
+      const queryOptions = {};
+      if (req.Model.modelName === "Category") {
+        queryOptions.populate = "parent";
+      }
+      const items = await req.Model.find().setOptions(queryOptions).limit(10);
+      res.send(items);
     }
-    const items = await req.Model.find().setOptions(queryOptions).limit(10);
-    res.send(items);
-  });
-  router.get('/:id', async (req, res) => {
+  );
+  router.get("/:id", async (req, res) => {
     const model = await req.Model.findById(req.params.id);
     res.send(model);
   });
 
-  app.use('/admin/api/rest/:resource', async (req, res, next) => {
-    const modelName = require('inflection').classify(req.params.resource);
-    req.Model = require(`../../models/${modelName}`);
-    next();
-  }, router);
+  app.use(
+    "/admin/api/rest/:resource",
+    async (req, res, next) => {
+      const modelName = require("inflection").classify(req.params.resource);
+      req.Model = require(`../../models/${modelName}`);
+      next();
+    },
+    router
+  );
 
-  const multer = require('multer');
-  const upload = multer({ dest: __dirname + '/../../uploads' });
+  const multer = require("multer");
+  const upload = multer({ dest: __dirname + "/../../uploads" });
 
-  app.post('/admin/api/upload', upload.single('file'), async (req, res) => {
+  app.post("/admin/api/upload", upload.single("file"), async (req, res) => {
     const file = req.file;
     file.url = `http://localhost:4000/uploads/${file.filename}`;
     res.send(file);
   });
 
-  app.post('/admin/api/login', async (req, res) => {
+  app.post("/admin/api/login", async (req, res) => {
     const { username, password } = req.body;
     // 根据用户名找用户
-    const User = require('../../models/AdminUser');
-    const user = await AdminUser.findOne({ username }).select('+password');
-    if (!user) {
-      return res.status(422).send({
-        message: '用户不存在'
-      });
-    }
+
+    const user = await AdminUser.findOne({ username }).select("+password");
+    assert(user, 422, "用户不存在");
 
     // 校验密码
-    const isValid = require('bcrypt').compareSync(password, user.password);
-    if (!isValid) {
-      return res.status(422).send({
-        message: '密码错误'
-      });
-    }
+    const isValid = require("bcrypt").compareSync(password, user.password);
+    assert(isValid, 422, "密码错误");
 
     // 返回token
-    const jwt = require('jsonwebtoken');
-    const token = jwt.sign({ id: user._id }, app.get('secret'));
+
+    const token = jwt.sign({ id: user._id }, app.get("secret"));
     res.send({ token });
+  });
+
+  // 错误处理函数
+  app.use(async (err, req, res, next) => {
+    res.status(err.statusCode || 500).send({
+      message: err.message,
+    });
   });
 };
