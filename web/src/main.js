462a0bf25b2f39543964be3157df4298e0103551
wqtitan <wqtitan@gmail.com>
2020-06-13 18:29:31 +0800

    设置样式

--- web/src/main.js
+++ web/src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from "vue";
+import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
+import "./style.scss";
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount("#app");
